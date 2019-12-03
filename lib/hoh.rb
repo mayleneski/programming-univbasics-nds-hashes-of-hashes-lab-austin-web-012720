c3ff2b42193fdd81aa831ae04fd05f5ae8a2c5c6
Maylene Poulsen <mayleneski@hotmail.com>
2019-12-03 19:17:32 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -52,20 +52,4 @@ naming_system = {
     }
   }
 }
-
-
-
-
-  
-  # Remember:
-  #  Kingdom
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
-  # So, if we have the "Kingdom" node we should be able to "tunnel" into the
-  # HoH all the way to Species!
-
 end
